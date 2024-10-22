9cb18e8cec0b433177393a2f284a62374efe68d4
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-10-22 20:36:57 -0300

    tentativa de colocar paginação

--- anime/src/app/Anime/page.tsx
+++ anime/src/app/Anime/page.tsx
@@ -5,20 +5,37 @@ import { AnimeProps } from "@/Types";
 
 export default function Anime() {
     const [animes, setAnimes] = useState<AnimeProps[]>([]);
+    const [paginaAtual, setPaginaAtual] = useState(1);
+    const [hasNextPage, setHasNextPage] = useState(true);
+    const [lastVisiblePage, setLastVisiblePage] = useState(1);
 
     useEffect(() => {
         const fetchAnimes = async () => {
             try {
-                const response = await fetch('/api/animes');
+                const response = await fetch(`/api/animes?page=${paginaAtual}`);
                 const data = await response.json();
                 setAnimes(data.data);
+                setHasNextPage(data.has_next_page);
+                setLastVisiblePage(data.last_visible_page);
             } catch (error) {
                 console.error('Erro ao buscar animes:', error);
             }
         };
 
         fetchAnimes();
-    }, []);
+    }, [paginaAtual]);
+
+    const proximaPagina = () => {
+        if (hasNextPage) {
+            setPaginaAtual(paginaAtual + 1);
+        }
+    }
+
+    const paginaAnterior = () => {
+        if (paginaAtual > 1) {
+            setPaginaAtual(paginaAtual - 1)
+        }
+    }
 
     return (
         <div>
@@ -33,6 +50,18 @@ export default function Anime() {
                         <p>Nota: {anime.score}</p>
                     </div>
                 ))}
+            </div>
+
+            <div>
+                <button onClick={paginaAnterior} disabled={paginaAtual === 1} >
+                    Anterior
+                </button>
+
+                <span> Página {paginaAtual} de {lastVisiblePage} </span>
+                
+                <button onClick={proximaPagina}>
+                    Próximo
+                </button>
             </div>
         </div>
     );
